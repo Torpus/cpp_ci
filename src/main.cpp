98c70fe25ecd543ac19221c7d35695437442ef74
Torpus <torpus@hotmail.com>
2018-10-20 10:02:13 -0500

    exploritory logging

--- src/main.cpp
+++ src/main.cpp
@@ -17,38 +17,47 @@ long getCurrentTime()
 void processInput()
 {
   this_thread::sleep_for(std::chrono::milliseconds(1));
-  cout << "processInput()\n";
 }
 void update()
 {
   this_thread::sleep_for(std::chrono::milliseconds(3));
-  cout << "update()\n";
 }
 void render(double lag) {
   int wait = lag/MS_PER_UPDATE;
   this_thread::sleep_for(std::chrono::milliseconds(wait));
-  cout << "render()\n";
 }
 
 int main()
 {
   double previous = getCurrentTime();
+  double startTime = getCurrentTime();
   double lag = 0.0;
-  for( int a = 0; a < 10; a = a + 1 ) {
+  int renderCount = 0;
+  int updateCount = 0;
+  int processInputCount = 0;
+
+  for( int a = 0; a < 1000; a = a + 1 ) {
     double current = getCurrentTime();
     double elapsed = current - previous;
     previous = current;
     lag += elapsed;
     cout << "lag: " << lag << "\n";
-    cout <<"ms_per_update: " << MS_PER_UPDATE << "\n";
     processInput();
+    processInputCount++;
 
     while (lag >= MS_PER_UPDATE)
     {
       update();
+      updateCount++;
       lag -= MS_PER_UPDATE;
     }
 
     render(lag / MS_PER_UPDATE);
+    renderCount++;
+    if( getCurrentTime() - startTime > 1000) {
+      cout << "input cycles per second: " << processInputCount;
+      cout << "render cycles per seconds " << renderCount;
+      cout << "update cycles per seconds " << updateCount;
+    }
   }
 }
