bffa04c15bddd89ec3077b183ff94b3e5ca603a2
Torpus <torpus@hotmail.com>
2018-12-04 17:24:51 -0600

    working on loop leak issue

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,19 @@ double MS_PER_UPDATE = 16.0;
 
 long getCurrentTime()
 {
-  clock_t t = clock();
-  return t;
+  return chrono::duration_cast< chrono::milliseconds >( chrono::system_clock::now().time_since_epoch() ).count();
 }
 
 void processInput()
 {
-  this_thread::sleep_for(std::chrono::milliseconds(1));
+  this_thread::sleep_for(chrono::milliseconds(1));
 }
 void update()
 {
-  this_thread::sleep_for(std::chrono::milliseconds(1));
+  this_thread::sleep_for(chrono::milliseconds(1));
 }
 void render() {
-  this_thread::sleep_for(std::chrono::milliseconds(1));
+  this_thread::sleep_for(chrono::milliseconds(1));
 }
 
 bool checkSecond(double time) {
@@ -31,15 +30,15 @@ bool checkSecond(double time) {
 
 int main()
 {
-  double previous = getCurrentTime();
-  double startTime = getCurrentTime();
+  long previous = getCurrentTime();
+  long startTime = getCurrentTime();
   double lag = 0.0;
   int renderCount = 0;
   int updateCount = 0;
   int processInputCount = 0;
 
   for( int a = 0; a < 100; a = a + 1 ) {
-    double elapsed = getCurrentTime() - previous;
+    long elapsed = getCurrentTime() - previous;
     cout << "elapsed(since last loop): " << elapsed << "\n";
     cout << "lag(main loop start value): " << lag << "\n";
     previous = getCurrentTime();
@@ -47,7 +46,7 @@ int main()
     processInput();
     processInputCount++;
 
-    while (lag >= MS_PER_UPDATE)
+    while ( lag >= MS_PER_UPDATE )
     {
       update();
       updateCount++;
@@ -57,7 +56,7 @@ int main()
     render();
     renderCount++;
 
-    if(checkSecond(startTime)) {
+    if( checkSecond(startTime) ) {
       cout << "input cycles per second: " << processInputCount << "\n";
       cout << "render cycles per seconds " << renderCount << "\n";
       cout << "update cycles per seconds " << updateCount << "\n";
